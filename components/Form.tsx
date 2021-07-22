7d9f38997d8e8a706c782551d026499af857ef7d
Valerius <10323945+valerius21@users.noreply.github.com>
2021-07-22 17:51:56 +0200

    finished form

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,9 +1,14 @@
 import React, { FC, useEffect } from 'react';
-import { useForm, Controller, FieldValues, Control } from 'react-hook-form';
-import { Button, Text, Switch, FormLabel, Checkbox, FormControl, Select, Box } from "@chakra-ui/react";
+import { Box, Button, Stack, Radio } from "@chakra-ui/react";
 import { useRouter } from 'next/router';
+import { Formik, useFormikContext } from 'formik';
+import { CheckboxControl, CheckboxContainer, RadioGroupControl } from "formik-chakra-ui";
+import * as Yup from 'yup'
 
-
+const validateSchema = Yup.object({
+	sensitivity: Yup.string().required(),
+	targetDemographic: Yup.array().min(1),
+});
 interface FormProps {
 	pageNumber: number;
 	uid: string;
@@ -13,121 +18,71 @@ const Form: FC<FormProps> = ({ pageNumber, uid }) => {
 	const nextPage = `/${uid}/${pageNumber + 1}`
 	const router = useRouter()
 
-	const defaultValues = {
-		Aquantance: false,
-		Collegues: false,
-		Family: true,
-		Friends: false,
-		Nobody: false,
-		sensitiviyLevel: "private",
-	}
-
-
-
-	const {
-		register, control, handleSubmit, reset,
-		formState: { errors, isSubmitting, isSubmitSuccessful }
-	} = useForm({
-		defaultValues
-	})
-
+	// prefetching the next page
 	useEffect(() => {
 		const { number } = router.query
-		let pageNumber = parseInt(number as string)
-		console.log(pageNumber)
-		if (pageNumber > 100) {
-			console.log('bruh');
-
+		let pNo = parseInt(number as string)
+		if (pNo > 100) {
+			// redirect to done after 100 submits
 			router.push('/done')
 			return
 		}
 		router.prefetch(nextPage);
 	}, [nextPage, router])
 
-	useEffect(() => {
-		if (isSubmitSuccessful) {
-			console.log('resetting');
-
-			reset({
-				Aquantance: false,
-				Collegues: false,
-				Family: true,
-				Friends: false,
-				Nobody: false,
-				sensitiviyLevel: "private",
-			})
-
-			router.push(nextPage)
-		}
-
-	}, [reset, isSubmitSuccessful, defaultValues, router, nextPage])
-
-
-	const onSubmit = (data: any) => {
-		console.log(data)
-		console.log(errors);
-	}
-
 	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<FormControl id="evaluation">
-				<Box m={4}>
-					<FormLabel mt={4}>
-						Classify the picture regarding its sensitivity.
-					</FormLabel>
-					<Controller
-						name="sensitiviyLevel"
-						control={control}
-						defaultValue=""
-						render={
-							({ field }) => (
-								<Select placeholder="Select sensitivity" {...field}>
-									<option value="very_private">very private</option>,
-									<option value="private">private</option>,
-									<option value="not_private">not private</option>,
-									<option value="not_possible">not possible</option>,
-								</Select>
-							)}
-					/>
-					<FormLabel mt={4}>
-						With whom would you be most likely to share this picture?
-					</FormLabel>
-					<Checkbox   {...register('Friends')} >
-						Friends
-					</Checkbox>
-					<br />
-					<Checkbox   {...register('Aquantance')} >
-						Aquantance
-					</Checkbox>
-					<br />
-					<Checkbox   {...register("Collegues", {})} >
-						Collegues
-					</Checkbox>
-					<br />
-					<Checkbox   {...register("Family", {})} >
-						Family
-					</Checkbox>
-					<br />
-					<Checkbox   {...register("Nobody", {})} >
-						Nobody
-					</Checkbox>
-					<br />
-				</Box>
-				<Button
-					m={4}
-					size="md"
-					rounded="full"
-					color="white"
-					bg="red.400"
-					_hover={{ bg: 'red.300' }}
-					isLoading={isSubmitting}
-					type="submit"
+		<Formik
+			initialValues={{
+				sensitivity: "",
+				targetDemographic: [],
+			}}
+
+			onSubmit={(values, { resetForm }) => {
+				console.log('values', values)
+				resetForm()
+				router.push(nextPage)
+			}}
+			validationSchema={validateSchema}
+		>
+			{({ handleSubmit, handleReset, isSubmitting }) => (
+				<Box
+					as="form"
+					px={5}
+					onSubmit={handleSubmit as any}
 				>
-					Submit
-				</Button>
-				<Button onClick={() => reset(defaultValues)}>Reset</Button>
-			</FormControl>
-		</form>
+					<RadioGroupControl mt={4} name="sensitivity" label="Classify the picture regarding its sensitivity.">
+						<Stack spacing="1">
+							<Radio value="1">very private</Radio>
+							<Radio value="2">private</Radio>
+							<Radio value="3">not private</Radio>
+						</Stack>
+					</RadioGroupControl>
+					<CheckboxContainer mt={4} name="targetDemographic" label="With whom would you be most likely to share this picture?">
+						<Stack spacing="1">
+							<CheckboxControl name="targetDemographic" value="Friends">Friends</CheckboxControl>
+							<CheckboxControl name="targetDemographic" value="Aquantance">Aquantance</CheckboxControl>
+							<CheckboxControl name="targetDemographic" value="Collegues">Collegues</CheckboxControl>
+							<CheckboxControl name="targetDemographic" value="Family">Family</CheckboxControl>
+							<CheckboxControl name="targetDemographic" value="Nobody">Nobody</CheckboxControl>
+						</Stack>
+					</CheckboxContainer>
+					<Stack my={5} spacing={20} direction="row">
+						<Button
+							size="md"
+							rounded="full"
+							color="white"
+							bg="red.400"
+							_hover={{ bg: 'red.300' }}
+							type="submit"
+							isLoading={isSubmitting}
+						>
+							Submit
+						</Button>
+						<Button onClick={handleReset}>Reset</Button>
+					</Stack>
+				</Box>
+			)}
+		</Formik>
 	);
 }
 
