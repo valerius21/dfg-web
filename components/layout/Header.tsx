535a857e92fb9c365cb8c0dc142261a6491b64cc
Valerius Mattfeld <mail@valerius.me>
2021-10-28 13:08:10 +0200

    language button

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -7,12 +7,22 @@ import { useTranslation } from 'react-i18next'
 const Header = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure()
 	const { t } = useTranslation();
+	const { i18n } = useTranslation()
 
 	return (
 		<Flex justifyContent={'flex-end'}>
+			<Button
+				size='sm'
+				m={3}
+				onClick={() => {
+					const lang = i18n.language == 'en' ? 'de' : 'en';
+					i18n.changeLanguage(lang)
+				}}>Sprache / Language</Button>
+
 			<Button size="sm" m={3} onClick={onOpen}>
 				{t("loginTitle")}
 			</Button>
+
 
 			<Modal isOpen={isOpen} onClose={onClose}>
 				<ModalOverlay />
