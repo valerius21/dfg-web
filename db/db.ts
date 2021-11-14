a2323030fe02189859cf47878221163886227335
Valerius Mattfeld <mail@valerius.me>
2021-11-14 16:17:04 +0100

    add more STUDY_SIZE refactoring

--- db/db.ts
+++ db/db.ts
@@ -1,176 +1,201 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client/core"
-import fetch from 'cross-fetch'
-import _ from "lodash"
-import { aggregation, insert_submission, private_images_query, public_images_query } from "./queries"
-import { exp_is_uuid_private } from "./exp_submission"
-import { checkSubmission, Submission } from "./submission"
-import { ImageInterface } from "./types"
-import { logger } from "utils/logger"
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client/core";
+import fetch from "cross-fetch";
+import _ from "lodash";
+import {
+  aggregation,
+  insert_submission,
+  private_images_query,
+  public_images_query,
+} from "./queries";
+import { exp_is_uuid_private } from "./exp_submission";
+import { checkSubmission, Submission } from "./submission";
+import { ImageInterface } from "./types";
+import { logger } from "utils/logger";
 
-
-if (!process.env.NEXT_GRAPHQL_URL) {
-	logger.error("GRAPHQL_URL is not set")
-	process.exit(1)
+if (!process.env.NEXT_PUBLIC_STUDY_SIZE) {
+  logger.fatal("NEXT_PUBLIC_STUDY_SIZE not set");
+  process.exit(1);
 }
 
-if (!process.env.NEXT_IMAGE_SERVER) {
-	logger.error("IMAGE_SERVER is not set")
-	process.exit(1)
+if (!process.env.NEXT_PUBLIC_GRAPHQL_URL) {
+  logger.fatal("NEXT_PUBLIC_GRAPHQL_URL is not set");
+  process.exit(1);
 }
 
-const GRAPHQL_URL: string = process.env.NEXT_GRAPHQL_URL
-const IMAGE_SERVER: string = process.env.NEXT_IMAGE_SERVER
+if (!process.env.NEXT_PUBLIC_IMAGE_SERVER) {
+  logger.fatal("NEXT_PUBLIC_IMAGE_SERVER is not set");
+  process.exit(1);
+}
 
+const STUDY_SIZE: number = parseInt(process.env.NEXT_PUBLIC_STUDY_SIZE);
+const STUDY_SIZE_HALF: number = Math.floor(STUDY_SIZE / 2);
+const GRAPHQL_URL: string = process.env.NEXT_PUBLIC_GRAPHQL_URL;
+const IMAGE_SERVER: string = process.env.NEXT_PUBLIC_IMAGE_SERVER;
 
 export const Client = new ApolloClient({
-	link: new HttpLink({ uri: GRAPHQL_URL, fetch }),
-	cache: new InMemoryCache(),
-})
+  link: new HttpLink({ uri: GRAPHQL_URL, fetch }),
+  cache: new InMemoryCache(),
+});
 
 export const getPrivateImages = async () => {
-	const { data } = await Client.query({
-		query: private_images_query
-	})
-	return data.private
-}
+  const { data } = await Client.query({
+    query: private_images_query,
+  });
+  return data.private;
+};
 
 export const getPublicImages = async () => {
-	const { data } = await Client.query({
-		query: public_images_query
-	})
-	return data.public
-}
+  const { data } = await Client.query({
+    query: public_images_query,
+  });
+  return data.public;
+};
 
 export const getRandomImageSet = async (): Promise<ImageInterface> => {
-	let privateImages = await getPrivateImages()
-	let publicImages = await getPublicImages()
+  let privateImages = await getPrivateImages();
+  let publicImages = await getPublicImages();
 
-	privateImages = _.shuffle(_.uniqBy(privateImages, 'id'))
-	publicImages = _.shuffle(_.uniqBy(publicImages, 'id'))
+  privateImages = _.shuffle(_.uniqBy(privateImages, "id"));
+  publicImages = _.shuffle(_.uniqBy(publicImages, "id"));
 
-	privateImages = _.take(privateImages, 50)
-	publicImages = _.take(publicImages, 50)
+  privateImages = _.take(privateImages, STUDY_SIZE_HALF);
+  publicImages = _.take(publicImages, STUDY_SIZE_HALF);
 
-	let randoms = _.shuffle([...privateImages, ...publicImages])
+  let randoms = _.shuffle([...privateImages, ...publicImages]);
 
-	if (randoms.length < 100) {
-		throw new Error("Less than 100 images!");
-	}
+  if (randoms.length < STUDY_SIZE) {
+    throw new Error(`Less than ${STUDY_SIZE} images!`);
+  }
 
-	randoms = randoms.map((image: any) => {
-		const url = `${IMAGE_SERVER}/${image.__typename === 'private' ? 'private' : 'public'}/${image.filename}`
-		const id = image.id
+  randoms = randoms.map((image: any) => {
+    const url = `${IMAGE_SERVER}/${
+      image.__typename === "private" ? "private" : "public"
+    }/${image.filename}`;
+    const id = image.id;
 
-		return {
-			id,
-			url
-		}
-	})
+    return {
+      id,
+      url,
+    };
+  });
 
-	return { images: randoms }
-}
+  return { images: randoms };
+};
 
-const determineURL = async (uuid: string, privateImages: any[], publicImages: any[]): Promise<string> => {
-	const isPrivate = await exp_is_uuid_private(uuid)
+const determineURL = async (
+  uuid: string,
+  privateImages: any[],
+  publicImages: any[]
+): Promise<string> => {
+  const isPrivate = await exp_is_uuid_private(uuid);
 
-	if (isPrivate) {
-		const { filename: file } = privateImages.find((image: any) => image.id === uuid)
-		return `${IMAGE_SERVER}/private/${file}`
-	}
-	const { filename } = publicImages.find((image: any) => image.id === uuid)
-	return `${IMAGE_SERVER}/public/${filename}`
-}
+  if (isPrivate) {
+    const { filename: file } = privateImages.find(
+      (image: any) => image.id === uuid
+    );
+    return `${IMAGE_SERVER}/private/${file}`;
+  }
+  const { filename } = publicImages.find((image: any) => image.id === uuid);
+  return `${IMAGE_SERVER}/public/${filename}`;
+};
 
 export const getAccumulatedSet = async (): Promise<ImageInterface> => {
-	const { data: doc } = await Client.query({
-		query: aggregation
-	})
+  const { data: doc } = await Client.query({
+    query: aggregation,
+  });
 
-	const { private: priv, public: publ } = doc
+  const { private: priv, public: publ } = doc;
 
-	// get all images
-	let privateImages = await getPrivateImages()
-	let publicImages = await getPublicImages()
+  // get all images
+  let privateImages = await getPrivateImages();
+  let publicImages = await getPublicImages();
 
-	// filter for 1 <= submissions <= 40
-	let _private = priv.filter((image: any) => (image.submissions_aggregate.aggregate.count) <= 40 && 1 <= image.submissions_aggregate.aggregate.count)
-	let _public = publ.filter((image: any) => (image.submissions_aggregate.aggregate.count) <= 40 && 1 <= image.submissions_aggregate.aggregate.count)
+  // filter for 1 <= submissions <= 40
+  let _private = priv.filter(
+    (image: any) =>
+      image.submissions_aggregate.aggregate.count <= 40 &&
+      1 <= image.submissions_aggregate.aggregate.count
+  );
+  let _public = publ.filter(
+    (image: any) =>
+      image.submissions_aggregate.aggregate.count <= 40 &&
+      1 <= image.submissions_aggregate.aggregate.count
+  );
 
-	// add the rest of the list to the filtered ones [<filtered>, <rest>]
-	_private = _private.concat(priv)
-	_public = _public.concat(publ)
+  // add the rest of the list to the filtered ones [<filtered>, <rest>]
+  _private = _private.concat(priv);
+  _public = _public.concat(publ);
 
-	// take the first 50
-	_private = _.take(_private, 50)
-	_public = _.take(_public, 50)
+  // take the first STUDY_SIZE_HALF
+  _private = _.take(_private, STUDY_SIZE_HALF);
+  _public = _.take(_public, STUDY_SIZE_HALF);
 
-	// check for uniqueness
-	_private = _.uniqBy(_private, 'id')
-	_public = _.uniqBy(_public, 'id')
+  // check for uniqueness
+  _private = _.uniqBy(_private, "id");
+  _public = _.uniqBy(_public, "id");
 
-	// shuffle all images
-	privateImages = _.shuffle(privateImages)
-	publicImages = _.shuffle(publicImages)
+  // shuffle all images
+  privateImages = _.shuffle(privateImages);
+  publicImages = _.shuffle(publicImages);
 
-	// concatinate randoms with uniques
-	_private = _private.concat(privateImages)
-	_public = _public.concat(publicImages)
+  // concatinate randoms with uniques
+  _private = _private.concat(privateImages);
+  _public = _public.concat(publicImages);
 
-	// take the first 50
-	_private = _.take(_private, 50)
-	_public = _.take(_public, 50)
+  // take the first 50
+  _private = _.take(_private, STUDY_SIZE_HALF);
+  _public = _.take(_public, STUDY_SIZE_HALF);
 
-	// check for uniqueness
-	_private = _.uniqBy(_private, 'id')
-	_public = _.uniqBy(_public, 'id')
+  // check for uniqueness
+  _private = _.uniqBy(_private, "id");
+  _public = _.uniqBy(_public, "id");
 
-	if (_private.length < 50 || _public.length < 50) {
-		throw new Error("Less than 50 images!")
-	}
+  if (_private.length < STUDY_SIZE_HALF || _public.length < STUDY_SIZE_HALF) {
+    throw new Error(`Less than ${STUDY_SIZE_HALF} images!`);
+  }
 
-	const res = _.shuffle([..._private, ..._public]).map(async image => {
-		return {
-			id: image.id,
-			url: await determineURL(image.id, privateImages, publicImages)
-		}
-	})
+  const res = _.shuffle([..._private, ..._public]).map(async (image) => {
+    return {
+      id: image.id,
+      url: await determineURL(image.id, privateImages, publicImages),
+    };
+  });
 
-	return { images: await Promise.all(res) }
-}
+  return { images: await Promise.all(res) };
+};
 
 const insertSubmission = async (submission: Submission) => {
-	const isPrivate = await exp_is_uuid_private(submission.id)
-	let publicID = null, privateID = null
+  const isPrivate = await exp_is_uuid_private(submission.id);
+  let publicID = null,
+    privateID = null;
 
-	if (isPrivate) {
-		privateID = submission.id
-	} else {
-		publicID = submission.id
-	}
+  if (isPrivate) {
+    privateID = submission.id;
+  } else {
+    publicID = submission.id;
+  }
 
-	const values = {
-		...submission,
-		private_picture: privateID,
-		public_picture: publicID,
-		is_private: isPrivate
-	}
+  const values = {
+    ...submission,
+    private_picture: privateID,
+    public_picture: publicID,
+    is_private: isPrivate,
+  };
 
-	return Client.mutate({
-		mutation: insert_submission,
-		variables: values
-	})
-}
-
+  return Client.mutate({
+    mutation: insert_submission,
+    variables: values,
+  });
+};
 
 export const addSubmission = async (submission: Submission): Promise<any> => {
-	if (!checkSubmission(submission)) {
-		return new Error("Invalid submission")
-	}
-	try {
-		return await insertSubmission(submission)
-	} catch (e) {
-		logger.error(e)
-		return e
-	}
-
-}
+  if (!checkSubmission(submission)) {
+    return new Error("Invalid submission");
+  }
+  try {
+    return await insertSubmission(submission);
+  } catch (e) {
+    logger.error(e);
+    return e;
+  }
+};
--- pages/annotate.tsx
+++ pages/annotate.tsx
@@ -1,169 +1,179 @@
-import { Box, Center, Code, Flex, Spacer } from '@chakra-ui/layout'
-import { Progress } from '@chakra-ui/progress'
-import { useColorModeValue, Text, Image, Stack } from '@chakra-ui/react'
-import { Spinner } from '@chakra-ui/spinner'
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
-import React, { useEffect } from 'react'
-import { atom, useRecoilState } from 'recoil'
-import useSWR from 'swr'
-import { logger } from 'utils/logger'
-import { fetcher } from 'utils/utils'
-import Form from '../components/Form'
-import Done from './done'
-
+import { Box, Center, Code, Flex, Spacer } from "@chakra-ui/layout";
+import { Progress } from "@chakra-ui/progress";
+import { useColorModeValue, Text, Image, Stack } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/spinner";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import React, { useEffect } from "react";
+import { atom, useRecoilState } from "recoil";
+import useSWR from "swr";
+import { logger } from "utils/logger";
+import { fetcher } from "utils/utils";
+import Form from "../components/Form";
+import Done from "./done";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-	return {
-		props: {
-			id: context.query.id as string,
-		}
-	}
-}
+  return {
+    props: {
+      id: context.query.id as string,
+    },
+  };
+};
 
 export interface Check {
-	pass: boolean,
-	imageID: string,
-	imageURL: string,
-	page: number,
-	checked: boolean,
-	isQuestionOne: boolean
+  pass: boolean;
+  imageID: string;
+  imageURL: string;
+  page: number;
+  checked: boolean;
+  isQuestionOne: boolean;
 }
 export interface AttentionCheck {
-	uid: string,
-	currentPage: number, // redundant
-	checks: Check[]
+  uid: string;
+  currentPage: number; // redundant
+  checks: Check[];
 }
 
 export const attentionCheckState = atom<AttentionCheck>({
-	key: 'att-check',
-	default: {
-		uid: '',
-		currentPage: -1,
-		checks: [{
-			pass: false,
-			imageID: 'att_0',
-			imageURL: 'https://c102-251.cloud.gwdg.de/public/4327805618_a2b1c48f5a.jpg',
-			page: 25,
-			checked: false,
-			isQuestionOne: true
-		},
-		{
-			pass: false,
-			imageID: 'att_1',
-			imageURL: 'https://c102-251.cloud.gwdg.de/private/4363222502_9828d7b93c.jpg',
-			checked: false,
-			page: 50,
-			isQuestionOne: true
-		},
-		{
-			pass: false,
-			imageID: 'att_2',
-			imageURL: 'https://c102-251.cloud.gwdg.de/private/4380717531_94abf4986c.jpg',
-			checked: false,
-			page: 75,
-			isQuestionOne: false
-		}]
-	}
-})
+  key: "att-check",
+  default: {
+    uid: "",
+    currentPage: -1,
+    checks: [
+      {
+        pass: false,
+        imageID: "att_0",
+        imageURL:
+          "https://c102-251.cloud.gwdg.de/public/4327805618_a2b1c48f5a.jpg",
+        page: 25,
+        checked: false,
+        isQuestionOne: true,
+      },
+      {
+        pass: false,
+        imageID: "att_1",
+        imageURL:
+          "https://c102-251.cloud.gwdg.de/private/4363222502_9828d7b93c.jpg",
+        checked: false,
+        page: 50,
+        isQuestionOne: true,
+      },
+      {
+        pass: false,
+        imageID: "att_2",
+        imageURL:
+          "https://c102-251.cloud.gwdg.de/private/4380717531_94abf4986c.jpg",
+        checked: false,
+        page: 75,
+        isQuestionOne: false,
+      },
+    ],
+  },
+});
 
-const Annotate = ({ id }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-	const colorMode = useColorModeValue('white', 'gray.800')
-	const { data, error, mutate } = useSWR(`/api/user?id=${id}`, fetcher)
-	const [attentionCheck, setAttentionCheck] = useRecoilState(attentionCheckState)
+const Annotate = ({
+  id,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const colorMode = useColorModeValue("white", "gray.800");
+  const { data, error, mutate } = useSWR(`/api/user?id=${id}`, fetcher);
+  const [attentionCheck, setAttentionCheck] =
+    useRecoilState(attentionCheckState);
 
-	// initialize att-check state
-	useEffect(() => {
-		if (id && !attentionCheck.uid) {
-			setAttentionCheck({
-				...attentionCheck,
-				uid: id,
-			})
-		}
-	}, [attentionCheck, id, setAttentionCheck])
+  // initialize att-check state
+  useEffect(() => {
+    if (id && !attentionCheck.uid) {
+      setAttentionCheck({
+        ...attentionCheck,
+        uid: id,
+      });
+    }
+  }, [attentionCheck, id, setAttentionCheck]);
 
-	// update att-check current page
-	useEffect(() => {
-		if (data && data.data && id && attentionCheck.currentPage === -1) {
-			const { users_by_pk: { next_index: nextIndex } } = data.data
-			setAttentionCheck({
-				...attentionCheck,
-				currentPage: nextIndex as number - 1
-			})
-		}
-	}, [attentionCheck, data, id, setAttentionCheck])
+  // update att-check current page
+  useEffect(() => {
+    if (data && data.data && id && attentionCheck.currentPage === -1) {
+      const {
+        users_by_pk: { next_index: nextIndex },
+      } = data.data;
+      setAttentionCheck({
+        ...attentionCheck,
+        currentPage: (nextIndex as number) - 1,
+      });
+    }
+  }, [attentionCheck, data, id, setAttentionCheck]);
 
+  if (error) {
+    logger.error(error);
+    return <>An Error Occured. Please Login or start a new session.</>;
+  }
+  if (!data) {
+    return (
+      <>
+        <Center mt="24">
+          <Spinner size="xl" />
+        </Center>
+      </>
+    );
+  }
 
-	if (error) {
-		logger.error(error)
-		return <>
-			An Error Occured. Please Login or start a new session.
-		</>
-	}
-	if (!data) {
-		return <>
-			<Center mt="24">
-				<Spinner size="xl" />
-			</Center>
-		</>
-	}
+  if (id) {
+    const {
+      users_by_pk: { images, next_index: nextIndex },
+    } = data.data;
+    if (nextIndex === 101) return <Done />;
 
-	if (id) {
-		const { users_by_pk: { images, next_index: nextIndex } } = data.data
-		if (nextIndex === 101) return <Done />
+    const { checks } = attentionCheck;
+    const check = checks.find(({ page }) => page === nextIndex - 1);
+    const isCheck = () => {
+      if (check) {
+        return !check.checked;
+      }
+      return false;
+    };
 
-		const { checks } = attentionCheck
-		const check = checks.find(({ page }) => page === nextIndex - 1)
-		const isCheck = () => {
-			if (check) {
-				return !check.checked
-			}
-			return false
-		}
+    return (
+      <>
+        <Progress colorScheme="red" value={nextIndex as number} />
+        <Flex w="full" alignItems="center" justifyContent="center">
+          <Box
+            bg={colorMode}
+            w="lg"
+            maxW="xl"
+            borderWidth="1px"
+            rounded="lg"
+            shadow="lg"
+            mt={5}
+            position="relative"
+          >
+            <Center p={5}>
+              <Stack spacing={3}>
+                <Text>UID:</Text>
+                <Code> {id}</Code>
+                <Text>Submission No.:</Text>
+                <Code>{(nextIndex as number) - 1}</Code>
+                <Spacer />
+                <Spacer />
+                <Image
+                  boxShadow="md"
+                  borderRadius="md"
+                  src={isCheck() ? check!.imageURL : images[nextIndex - 1].url}
+                  alt="Image"
+                />
+              </Stack>
+            </Center>
+            <Form
+              uid={id as string}
+              imageID={isCheck() ? check!.imageID : images[nextIndex - 1].id}
+              pageNumber={nextIndex as number}
+              refetch={mutate}
+              isCheck={isCheck()}
+            />
+          </Box>
+        </Flex>
+      </>
+    );
+  }
 
-		return (
-			<>
-				<Progress colorScheme="red" value={nextIndex as number} />
-				<Flex w="full" alignItems='center' justifyContent='center'>
-					<Box
-						bg={colorMode}
-						w="lg"
-						maxW="xl"
-						borderWidth="1px"
-						rounded="lg"
-						shadow="lg"
-						mt={5}
-						position="relative"
-					>
-						<Center p={5}>
-							<Stack spacing={3}>
-								<Text>UID:</Text>
-								<Code> {id}</Code>
-								<Text>Submission No.:</Text>
-								<Code>{nextIndex as number - 1}</Code>
-								<Spacer />
-								<Spacer />
-								<Image
-									boxShadow="md"
-									borderRadius='md'
-									src={isCheck() ? check!.imageURL : images[nextIndex - 1].url}
-									alt='Image'
-								/>
-							</Stack>
-						</Center>
-						<Form
-							uid={id as string}
-							imageID={isCheck() ? check!.imageID : images[nextIndex - 1].id}
-							pageNumber={nextIndex as number}
-							refetch={mutate}
-							isCheck={isCheck()}
-						/>
-					</Box>
-				</Flex>
-			</>
-		)
-	}
+  return <div>ID needed! Please login or start a new session.</div>;
+};
 
-	return <div>ID needed! Please login or start a new session.</div>
-}
-
-export default Annotate
+export default Annotate;
